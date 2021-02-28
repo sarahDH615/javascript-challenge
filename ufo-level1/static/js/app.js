ca6867bdb908615941cdd75edf50e443be27d071
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2021-02-27 20:21:10 -0500

    adding event handlers

--- ufo-level1/static/js/app.js
+++ ufo-level1/static/js/app.js
@@ -1,4 +1,25 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// defining the input field
+var form_box = d3.select('#datetime');
+
+// defining the button
+var button = d3.select('#filter-btn')
+
+// defining the form
+var form = d3.select('form')
+
+// var tr = d3.select('tbody').append('tr');
+
+// tr.append('td').text()
+function fillTable() {
+    // prevent page reload
+    d3.event.preventDefault();
+    console.log(form_box.node().value);
+}
+// event listeners
+    // for keyup in the input box
+form.on('submit', fillTable);
+    // for click of button
+button.on('click', fillTable);
